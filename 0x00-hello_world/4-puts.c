43df146d0c7b3adfdc22b4e69f371b42795e3e51
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-13 12:26:39 -0700

    c prgramming 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /*
  *puts usage
  */
-int main(void) 
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
