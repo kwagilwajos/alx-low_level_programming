55a3fed43fa0663373eaf6d324cd46ba5e576d30
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-13 12:38:14 -0700

    c programming 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(){
+/*
+ * The main function 
+ * shows the use of printf
+ * returns 0 if everything is okay
+ */
+int main()
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
