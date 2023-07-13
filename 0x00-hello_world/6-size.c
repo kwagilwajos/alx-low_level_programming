274db69bb59c8be307c97ab5ff202800b6582994
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-13 12:49:32 -0700

    c programming 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Size of char: %zu bytes(s)\n", sizeof(char));
-    printf("Size of int: %zu bytes(s)\n", sizeof(int));
-    printf("Size of long int: %zu bytes(s)\n", sizeof(long int));
-    printf("Size of long long int: %zu bytes(s)\n", sizeof(long long int));
-    printf("Size of float: %zu bytes(s)\n", sizeof(float));
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
 
     return 0;
 }
