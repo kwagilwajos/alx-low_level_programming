3a38dbb904635f96d0fb30facc1f78cae715481e
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-13 13:17:21 -0700

    c programming 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,17 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+/**
+ * main - function
+ * Return: 0 if everything is okay
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int stderr_fd = open("/dev/stderr", O_WRONLY);
 
-int main() {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int stderr_fd = open("/dev/stderr", O_WRONLY);
+write(stderr_fd, message, strlen(message));
+close(stderr_fd);
 
-    write(stderr_fd, message, strlen(message));
-    close(stderr_fd);
-
-    return 1;
+return (1);
 }
