06b230d875a3c39eb94b373640a2396e4ff084c8
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-14 14:34:37 -0700

    if and while 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,14 @@
 int main(void)
 {
 int n;
-int a;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-a = n % 10;
-if (a > 5)
-printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, a);
-
-if (a < 6 && a != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
-
-if (a == 0)
-printf("Last digit of %d is %d and is 0\n", n, a);
+if (n > 0)
+printf("%d is positive\n", n);
+if (n == 0)
+printf("%d is zero\n", n);
+if (n < 0)
+printf("%d is negative\n", n);
 
 return (0);
 }
