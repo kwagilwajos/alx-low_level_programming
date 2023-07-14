6e27a105851c6cee822b927d0708bf6787a89f9e
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-14 14:27:47 -0700

    if while 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 
 a = n % 10;
 if (a > 5)
-printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, a);
+printf("Last digit of %d is %d and is greater than 5\n", n, a);
 
 if (a < 6 && a != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
