f800a8b72e0134fe5c03601a83669339304e6a34
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-14 14:41:31 -0700

    shell if 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 if everything is okay
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+putchar('\n');
+return (0);
+}
