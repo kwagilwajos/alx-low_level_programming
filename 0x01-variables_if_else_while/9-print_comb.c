4e8db209e75758acdbca3368973e285d36ebc497
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-14 15:30:19 -0700

    shell if 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 if everything is okay
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+if (i == 9) break;
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
+
