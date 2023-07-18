35d4dcf5bfa50a0b12cc850952663292007ba25a
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 13:19:41 -0700

    fucntions 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - function
+ * @c: integer
+ * Return: 0 if everything is okay
+ */
+int _isalpha(int c)
+{
+return (isalpha(c) ? 1 : 0);
+
+}
