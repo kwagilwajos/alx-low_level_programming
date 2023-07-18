69e804decd96485c7ebc89a46e716937c8cecb6e
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 12:49:30 -0700

    functions 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function
+ * Return: 0 if everything is okay
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int x = 0;
+
+while (x<10)
+{
+print_alphabet();
+x++;
+}
+
+}
