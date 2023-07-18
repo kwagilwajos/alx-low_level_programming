771bfcde90ae10f26833ed93f6b9eb1bf462b5fa
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 12:56:03 -0700

    functions 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,11 @@
 void print_alphabet_x10(void)
 {
 int x = 0;
+char letter;
 
 while (x<10)
 {
-for (char letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 
 _putchar('\n');
