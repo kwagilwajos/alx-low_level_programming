092417fec581f43d8f2c4e6ed0c66ab04112e4f9
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 14:01:35 -0700

    functions 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,9 +4,11 @@
  */
 void jack_bauer(void)
 {
-for (int hour = 0; hour < 24; hour++)
+int hour;
+int minute;
+for (hour = 0; hour < 24; hour++)
 {
-for (int minute = 0; minute < 60; minute++)
+for (minute = 0; minute < 60; minute++)
 {
 _putchar('0' + hour / 10);
 _putchar('0' + hour % 10);
