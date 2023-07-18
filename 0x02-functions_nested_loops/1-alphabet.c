4495da25e9c65f78825b443a30b924da6072bb2a
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 12:35:36 -0700

    functions 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function
+ * Return: 0 if everything is okay
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+
+}
