7a6f251e2f9ec2c0054aca2a6bc66ac468aa964b
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 14:13:29 -0700

    functions 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,11 @@
  */
 void times_table(void)
 {
-for (int i = 0; i <= 9; i++)
+int i;
+int j;
+for (i = 0; i <= 9; i++)
 {
-for (int j = 0; j <= 9; j++)
+for (j = 0; j <= 9; j++)
 {
 int result = i * j;
 
