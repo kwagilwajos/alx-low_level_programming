5f9afb28e4a2c3fed478b2c7d8cde400b82fe60d
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 12:57:55 -0700

    functions 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - function
+ * print_alphabet_x10 - function
  * Return: 0 if everything is okay
  */
 void print_alphabet_x10(void)
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 int x = 0;
 char letter;
 
-while (x<10)
+while (x < 10)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
