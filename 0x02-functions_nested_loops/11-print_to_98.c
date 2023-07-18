0041cefca8c81f68fb03793a077f0a9605e653b6
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 14:36:35 -0700

    functions 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@ void print_to_98(int n)
 int i;
 if (n <= 98)
 {
-for (int i = n; i <= 98; i++)
+for (i = n; i <= 98; i++)
 {
 if (i != 98)
 {
@@ -26,7 +26,7 @@ _putchar(i % 10 + '0');
 }
 else
 {
-for (int i = n; i >= 98; i--)
+for (i = n; i >= 98; i--)
 {
 if (i != 98)
 {
