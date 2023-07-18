fb542036d59f5dddc87b6ab9cbb422336661849c
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-18 14:33:14 -0700

    functions 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ * @n: integer
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (int i = n; i <= 98; i++)
+{
+if (i != 98)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+}
+}
+else
+{
+for (int i = n; i >= 98; i--)
+{
+if (i != 98)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+}
+}
+_putchar('\n');
+}
