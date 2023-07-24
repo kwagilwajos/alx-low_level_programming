5a1f410f28d832b0cd9da3155a21eb9ab7306ab1
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-24 13:26:56 -0700

    8 pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 int length = 0;
 char *end = str;
 int i;
-int half_length = (length - 1) / 2;
+int half_length;
 while (*end != '\0')
 {
 length++;
@@ -17,6 +17,7 @@ end++;
 }
 
 
+half_length = (length - 1) / 2;
 for (i = half_length + 1; i < length; i++)
 {
 _putchar(str[i]);
