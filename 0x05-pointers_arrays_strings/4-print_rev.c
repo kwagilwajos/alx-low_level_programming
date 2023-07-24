9264976a937e1715922ca9b14da89fccedcbd6d2
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-24 13:01:44 -0700

    pointer 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,7 @@ void print_rev(char *s)
 
 int length = 0;
 char *end = s;
+int i;
 
 while (*end != '\0')
 {
@@ -15,7 +16,7 @@ length++;
 end++;
 }
 
-for (int i = length - 1; i >= 0; i--)
+for (i = length - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
