87d64b4c9516cd78de7bf66287b7e2a1a0e28845
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-24 13:39:35 -0700

    9 pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_array - function
+ * @a: integer
+ * @n: integer
+ */
+void print_array(int *a, int n)
+{
+
+int i;
+int num;
+int divisor;
+for (i = 0; i < n; i++)
+{
+num = a[i];
+
+if (num == 0)
+{
+_putchar('0');
+}
+else
+{
+if (num < 0)
+{
+_putchar('-');
+num = -num;
+}
+divisor = 1;
+while (num / divisor)
+divisor *= 10;
+
+divisor /= 10;
+while (divisor)
+{
+_putchar(num / divisor + '0');
+num %= divisor;
+divisor /= 10;
+}
+}
+if (i < n - 1)
+_putchar(',');
+}
+_putchar('\n');
+}
