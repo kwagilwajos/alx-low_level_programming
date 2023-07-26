512ead50f43314d89bcddecca1b040801a6b048d
kwagilwajos <josephkwagilwa@gmail.com>
2023-07-26 11:37:09 -0700

    6 pointers 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+/**
+ * _strncat - function
+ * @dest: integer
+ * @src: integer
+ * @n: integer
+ * Return: 0 if everything is okay
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *ptr = dest;
+while (*ptr != '\0')
+{
+ptr++;
+}
+
+while (*src != '\0' && n > 0)
+{
+*ptr = *src;
+ptr++;
+src++;
+n--;
+}
+
+*ptr = '\0';
+
+return (dest);
+}
